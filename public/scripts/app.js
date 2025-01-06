1a4614689934cb1c6f14a6734a8b3710134ebf80
Jaidenpearson <[jaidensrpearson@gmail.com]>
2025-01-05 18:44:15 -0800

    modal now appears when add-to-cart button is pressed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,12 @@
+$(document).ready(function() {
+//Nav bar menu toggle
 $('#menu-toggle').on('click', function() {
   $('.sub-nav').toggle(300)
   $('.fa-angle-up').toggle()
   $('.fa-angle-down').toggle()
 });
 
-
+//Course HTML to organize menu page
 const createCourseHTML = function(course) {
   let $course = $(`
     <section class="menu-categories resume-section" ">
@@ -16,6 +18,7 @@ const createCourseHTML = function(course) {
   return $course;
 };
 
+//Sorts courses so they render inproper order
 const createCourses = function(courses) {
   console.log('Creating courses:', courses); // Debugging line
 
@@ -42,6 +45,7 @@ const createCourses = function(courses) {
   });
 };
 
+//HTML for each dish
 const createDishHTML = function(dish) {
   let $dish = $(`
     <div class="menu-item-container">
@@ -49,7 +53,9 @@ const createDishHTML = function(dish) {
         <h3 class="mb-0">${dish.name}</h3>
         <div class="item-sub-container">
           <p>${dish.description}</p>
-          <button class="btn btn-outline-primary text-nowrap">Add to order</button>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Add to Cart
+            </button>
         </div>
       </div>
       <div class="dish-image-container"><img class="dish-image" src="${dish.images}" alt=""></div>
@@ -58,6 +64,7 @@ const createDishHTML = function(dish) {
   return $dish;
 };
 
+//Appends dishes to the proper course
 const createDishes = function(dishes) {
   dishes.forEach(dish => {
     const $dish = createDishHTML(dish);
@@ -65,6 +72,8 @@ const createDishes = function(dishes) {
   });
 }
 
+
+//AJAX request to create courses and then dishes on success
 $.ajax({
   url: '/api/courses',
   method: 'GET',
@@ -89,5 +98,36 @@ $.ajax({
 
 });
 
+//Dish modal that appears when click 'add to cart' button
 
+const createModal = function(dish) {
+  let $modal = $(`
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+`);
+    $('body').append($modal);
+    };
+
+createModal();
+
+});
+
+// $(document).on('click', '.btn-outline-primary', function() {
+//   $modal.appendTo('body').modal('show');
+// });
 
