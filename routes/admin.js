5be192d795e8aff93331c420feecaa010cdf235f
Gladdy17 <gladston.bosco@gmail.com>
2025-01-06 18:39:21 -0700

    fixed bug with DB_name

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { Pool } = require('pg');
-require('dotenv').config();
 
 // Configure the PostgreSQL connection
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     port: process.env.DB_PORT,
 });
 
