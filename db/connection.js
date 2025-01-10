6b9ea6b203510f702b70457425207403ff945f34
Gladdy17 <gladston.bosco@gmail.com>
2025-01-10 16:50:44 -0700

    testing prod deploy

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,25 @@
 // PG database client/connection setup
 const { Pool } = require('pg');
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
+// Check if DATABASE_URL is present (for production environments)
+const dbParams = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+    }
+  : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    };
 
 const db = new Pool(dbParams);
 
-db.connect();
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch((err) => console.error('Error connecting to the database:', err));
 
 module.exports = db;
+
